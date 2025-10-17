3e43153f42d2acf4045b4d1d2c3eb5631165cc21
CallMeLlms <jllamera06@gmail.com>
2025-10-17 09:54:33 +0800

    sir jp

--- basics/database/migrations/2025_10_17_014634_test_users.php
+++ basics/database/migrations/2025_10_17_014634_test_users.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         //
-
         Schema::create('users_table', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
