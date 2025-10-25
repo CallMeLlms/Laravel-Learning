29cbb50d1fd0e149a0b3ca64c4ad9d5a3bac66c9
Justine L. Llamera <jllamera06@gmail.com>
2025-10-25 22:52:52 +0800

    okage

--- basics/resources/views/component/form.blade.php
+++ basics/resources/views/component/form.blade.php
@@ -5,11 +5,12 @@
         <div class="m-form-container">
             <h1 class="h1-reusable">Sign in<h1>
                 
-            <div>
+            <div class="m-user-class">
                 <label>username</label>
                 <input/>
+            </div>
 
-
+            <div class="m-pass-user-class">
                 <label>passowrd</label>
                 <input/>
             </div>
